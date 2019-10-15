5ef0f5652399018b9f042407956e057d6f7e806d
liuju <301108603@qq.com>
2019-10-15 09:42:19 +0800

    修改定时任务时间

--- task/__init__.py
+++ task/__init__.py
@@ -8,14 +8,14 @@ app = Celery('my_task')
 app.config_from_object("task.celeryconfig")
 
 taskList = {
-    "tast1": {
-        'task': "task.apitestTask.everySeconds",
-        'schedule': timedelta(seconds=20),  # 每 30 秒执行一次
-        'args': ()  # 任务函数参数
-    },
+    # "tast1": {
+    #     'task': "task.apitestTask.everySeconds",
+    #     'schedule': timedelta(seconds=20),  # 每 30 秒执行一次
+    #     'args': ()  # 任务函数参数
+    # },
     "tast2": {
         'task': "task.apitestTask.everyDay",
-        'schedule': crontab(hour=9, minute=50),  # 每 30 秒执行一次
+        'schedule': crontab(hour=9, minute=40),  # 每 30 秒执行一次
         'args': ()  # 任务函数参数
     },
 }
